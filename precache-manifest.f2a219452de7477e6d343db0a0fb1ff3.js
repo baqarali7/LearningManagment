3393500f628262d81724f64149b07a7b4ad0d54f
Baqar Ali <baqarali789@gmail.com>
2020-05-05 05:08:33 +0500

    Updates

---
+++ precache-manifest.f2a219452de7477e6d343db0a0fb1ff3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b952d9fa08374c8895c0ec27f617f7",
+    "url": "/LearningManagment/index.html"
+  },
+  {
+    "revision": "552da6323dc053f1e9a4",
+    "url": "/LearningManagment/static/css/2.02c2feb7.chunk.css"
+  },
+  {
+    "revision": "912c130f51b6dab27647",
+    "url": "/LearningManagment/static/css/main.beb2c7e3.chunk.css"
+  },
+  {
+    "revision": "552da6323dc053f1e9a4",
+    "url": "/LearningManagment/static/js/2.7a67bef3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/LearningManagment/static/js/2.7a67bef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912c130f51b6dab27647",
+    "url": "/LearningManagment/static/js/main.151d628e.chunk.js"
+  },
+  {
+    "revision": "6c09b7a593b14bea26e7",
+    "url": "/LearningManagment/static/js/runtime-main.4f21da25.js"
+  },
+  {
+    "revision": "11b9e75924c9230d245ffc2d9b46ce5e",
+    "url": "/LearningManagment/static/media/addfriend.11b9e759.png"
+  },
+  {
+    "revision": "6ab100d4c31485f33558aebd0e8ff59d",
+    "url": "/LearningManagment/static/media/building.6ab100d4.png"
+  },
+  {
+    "revision": "ba053665be226c14d273c2489145836d",
+    "url": "/LearningManagment/static/media/head.ba053665.png"
+  },
+  {
+    "revision": "b812eda50e250c3d6762a4ca2808064c",
+    "url": "/LearningManagment/static/media/headphone.b812eda5.png"
+  },
+  {
+    "revision": "da4548a2f5756995db65d87561a45538",
+    "url": "/LearningManagment/static/media/iii.da4548a2.jpg"
+  },
+  {
+    "revision": "8fe6bc1398b8bbd132c6a0c3a45383a7",
+    "url": "/LearningManagment/static/media/iii2.8fe6bc13.jpg"
+  },
+  {
+    "revision": "670b5d6dfa78f610744e7fe82a190892",
+    "url": "/LearningManagment/static/media/iii3.670b5d6d.jpg"
+  },
+  {
+    "revision": "490b1395d51b39a37fbd9d46c96805a4",
+    "url": "/LearningManagment/static/media/iii4.490b1395.jpg"
+  },
+  {
+    "revision": "6f9c2cf0a941e7977181bab01d93a766",
+    "url": "/LearningManagment/static/media/iii5.6f9c2cf0.jpg"
+  },
+  {
+    "revision": "3d9a0828aff5a106c09a9232864b848d",
+    "url": "/LearningManagment/static/media/logolearn.3d9a0828.png"
+  },
+  {
+    "revision": "2c45e3eb233c090c1bf087c1db66d2df",
+    "url": "/LearningManagment/static/media/mail.2c45e3eb.png"
+  },
+  {
+    "revision": "ee98fd402cfa287a8867bc697d5dd7a5",
+    "url": "/LearningManagment/static/media/ooo.ee98fd40.jpg"
+  },
+  {
+    "revision": "21502895c39ea06866da328eb140a4a8",
+    "url": "/LearningManagment/static/media/ooo2.21502895.jpg"
+  },
+  {
+    "revision": "f2334460c755d8d04034718d7f3e294b",
+    "url": "/LearningManagment/static/media/people.f2334460.png"
+  },
+  {
+    "revision": "6b3cfd8d174bbbeda35dd9c9b07614df",
+    "url": "/LearningManagment/static/media/photo1.6b3cfd8d.jpg"
+  },
+  {
+    "revision": "d16800ff3fd65407f061f86e16c6e63e",
+    "url": "/LearningManagment/static/media/photo2.d16800ff.jpg"
+  },
+  {
+    "revision": "016b5584f69906078844cb59c79bff55",
+    "url": "/LearningManagment/static/media/photo3.016b5584.jpg"
+  },
+  {
+    "revision": "a17300edab97e8d09e3c68491fb3db16",
+    "url": "/LearningManagment/static/media/photo4.a17300ed.jpg"
+  },
+  {
+    "revision": "c9d92771343240525df42171c3ac5e72",
+    "url": "/LearningManagment/static/media/ppp.c9d92771.jpg"
+  }
+]);
